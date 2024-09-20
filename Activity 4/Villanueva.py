35f5a4ff45b812bd340ca5624264f04a9c8ab4d2
Luke Richard V. Villanueva <Lukerichard.villanueva@gmail.com>
2024-09-20 03:07:28 -0700

    Create Villanueva.py

---
+++ Activity 4/Villanueva.py
@@ -0,0 +1,12 @@
+sampleText1 = "My name is {}. I love {}. I love playing {}"
+sampleText1a = sampleText1.format("Luke","Jollibee", "Mir4")
+print(sampleText1a)
+
+
+#sampleText2 = "My name is {1}. I love {2}. I love playing {0}."
+#sampleText2a = sampleText2.format("Mir4", "", "", "Ros", "Borgarr")
+#print(sampleText2a)
+
+#sampleText3 = "My name is {name}. I love {Food}. I love playing {play}."
+#sampleText3a = sampleText3.format(Food="Jollibee", play="Mir4", name="Luke")
+#print(sampleText3a)
